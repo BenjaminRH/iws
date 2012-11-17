7c7a98ff910be05b8df658556ada8336ca2b1dae
Benjamin Harris <benjamin3harris@gmail.com>
2012-11-17 18:43:56 +0200

    Add base WYSIWYG editor

--- application/views/post/edit.blade.php
+++ application/views/post/edit.blade.php
@@ -1,1 +1,12 @@
-This is the /home/benjamin/Desktop/iws_laravel/application/views/post/edit.blade.php view
+<script src="js/nicEdit.js"></script>
+<script>
+	new nicEditor({
+		buttonList : [
+			'bold',
+			'italic',
+			'underline',
+			'strikeThrough',
+			'YouTube'
+		]
+	}).panelInstance('body');
+</script>
