fe671daddeeb98e9897f002899b44ce0ffe0d908
Benjamin Harris <benjamin3harris@gmail.com>
2012-11-18 22:11:35 +0200

    Update navigation

--- application/views/layouts/footer.blade.php
+++ application/views/layouts/footer.blade.php
@@ -1,2 +1,8 @@
 <hr>
 <p>Copyright 2012 by Miriam Kalman and Benjamin Harris. All rights reserved.</p>
+
+@if (Auth::guest())
+<p>{{ HTML::link('login', 'Login') }}</p>
+@else
+<p>Currently logged in as <b>{{ Auth::user()->email }}</b> | {{ HTML::link('logout', 'Logout') }}</p>
+@endif
