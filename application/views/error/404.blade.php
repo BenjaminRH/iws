598a5a514aae5ccbe2478b9e5022d5445bed282b
Benjamin Harris <benjamin3harris@gmail.com>
2012-12-26 16:24:32 +0200

    Fix error pages

--- application/views/error/404.blade.php
+++ application/views/error/404.blade.php
@@ -4,6 +4,12 @@
 <title>404 - Page not found</title>
 @endsection
 
+@section('page_title')
+<div class="sixteen columns">
+	<h1 class="page-title">404 - Page not found<span class="line"></span></h1>
+</div>
+@endsection
+
 @section('content')
 {{ HTML::image('http://i1246.photobucket.com/albums/gg609/alyoshakaramazov1/Dr-Horrible_zps7840f0c6.jpg', '404') }}
 @endsection
--- application/views/error/500.blade.php
+++ application/views/error/500.blade.php
@@ -4,6 +4,12 @@
 <title>500 - Internal Server Error</title>
 @endsection
 
+@section('page_title')
+<div class="sixteen columns">
+	<h1 class="page-title">404 - Page not found<span class="line"></span></h1>
+</div>
+@endsection
+
 @section('content')
 <p>Yeah, sorry about that. There seems to be some problem with our server.</p>
 @endsection
